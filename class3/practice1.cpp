d1aebea20e2fb40db37470288f4093a1e2e0f168
divya-racha <divyaracha02@gmail.com>
2025-10-04 12:50:37 -0500

    practice one solution

--- class3/practice1.cpp
+++ class3/practice1.cpp
@@ -1,1 +1,27 @@
 // Print all the armstrong numbers from 1 to 100000 using functions
+#include <iostream>
+#include <cmath>
+
+// the count digits
+int countDigits(int num){
+    int count = 0;
+    while (num > 0) {
+        count++;
+        num /= 10;
+    }
+    return (count == 0) ? 1 : count; 
+}
+
+bool isArmstrong(int num) {
+    int original = num;
+    int digits = countDigits(num);
+    int sum = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        sum += pow(digit, digits);
+        num /= 10;
+    }
+
+    return
+}
